91603444bffc341258709657432b16c77d1442d6
Javier Aguirre <contacto@javaguirre.net>
2020-10-09 11:32:42 +0200

    Added ftp to locker wp

--- terraform/locker.tf
+++ terraform/locker.tf
@@ -7,10 +7,32 @@ resource "aws_instance" "locker_wordpress" {
     aws_security_group.web.id,
     aws_security_group.ssh.id,
     aws_security_group.egress-tls.id,
+    aws_security_group.ftp.id,
     aws_security_group.ping-ICMP.id
   ]
 
   tags = {
     Name = "locker-wordpress"
+  }
+}
+
+resource "aws_security_group" "ftp" {
+  name        = "ftp"
+  description = "FTP security group"
+
+  ingress {
+    from_port        = 21
+    to_port          = 21
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
