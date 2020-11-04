d39b21a4182d8c823527b414608ae8b6e31e0280
Javier Aguirre <contacto@javaguirre.net>
2020-11-04 12:32:51 +0100

    Added locker-wp database

--- terraform/locker.tf
+++ terraform/locker.tf
@@ -1,3 +1,15 @@
+variable "locker_wp_db_username" {
+  default = ""
+}
+
+variable "locker_wp_db_name" {
+  default = ""
+}
+
+variable "locker_wp_db_password" {
+  default = ""
+}
+
 resource "aws_instance" "locker_wordpress" {
   ami           = var.wordpress_ami
   instance_type = var.ec2_size
@@ -37,6 +49,23 @@ resource "aws_security_group" "ftp" {
   }
 }
 
+resource "aws_db_instance" "locker_wp_db" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t2.medium"
+  name                 = var.locker_wp_db_name
+  username             = var.locker_wp_db_username
+  password             = var.locker_wp_db_password
+  parameter_group_name = "default.mysql8.0"
+  publicly_accessible  = true
+}
+
 output "locker-wordpress" {
   value = "A record: ${aws_instance.locker_wordpress.public_dns}"
+}
+
+output "locker-wp-db" {
+  value = "mysql://${aws_db_instance.locker_wp_db.username}:${aws_db_instance.locker_wp_db.password}@${aws_db_instance.locker_wp_db.endpoint}/${aws_db_instance.locker_wp_db.name}"
 }
